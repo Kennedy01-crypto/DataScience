17a422917684974fa3581a737f9683d6f327da4f
Jim Kennedy <kennedyomata@gmail.com>
2025-03-26 13:01:00 +0300

    added more statistics methods

--- R_Projects/DataVisualization/StatisticsIntro.R
+++ R_Projects/DataVisualization/StatisticsIntro.R
@@ -25,3 +25,36 @@ print(sort(Data_Cars$cyl)) # in ascending order
 # Analyzing the data
 # Use the summary() function to get a summary of the data set
 print(summary(Data_Cars))
+
+# ----------------------------------------------------------------
+
+# MAX MIN
+# find the highst and lowest value of variable hp
+print(max(Data_Cars$hp)) #335
+print(min(Data_Cars$hp)) #52
+
+# find the index position of the max and min value
+print(which.max(Data_Cars$hp)) # 32
+print(which.min(Data_Cars$hp)) # 19
+
+# combine which.max() and which.min() with the rownames() function to get the 
+# name of the car with the largest and smallest horsepower:
+print(rownames(Data_Cars)[which.max(Data_Cars$hp)]) # Output: "Maserati Bora"
+print(rownames(Data_Cars)[which.min(Data_Cars$hp)]) # Output: "Honda Civic"
+
+# --------------------------------------------------------------
+# MEAN MEDIAN MODE
+# Mean
+# Calculate the mean of the variable weight(wt)
+print(mean(Data_Cars$wt)) # 3.21725
+
+# Median
+# Calculate the median of the variable weight(wt)
+print(median(Data_Cars$wt)) # 3.325
+
+# Mode
+# Calculate mode(wt)
+print(names(sort(-table(Data_Cars$wt)))[1])
+
+# ---------------------------------------------------
+Percentiles
